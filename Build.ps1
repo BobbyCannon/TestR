4edc3d51c3a29a64f3c1a693fc9bc9f0a9ea1566
Bobby Cannon <bobby.cannon@live.com>
2015-08-02 16:13:59 -0400

    Updated documentation.

--- Build.ps1
+++ Build.ps1
@@ -1,6 +1,13 @@
-﻿$template = Get-Content .\template.html -Raw
+﻿$scriptPath = Split-Path (Get-Variable MyInvocation).Value.MyCommand.Path 
+Push-Location $scriptPath
+
+Import-Module MarkR.PowerShell
+
+$template = Get-Content .\template.html -Raw
 $content = Get-Content .\index.md -Raw | Convert-Markdown
 Set-Content .\index.html $template.Replace("[Content]", $content)
 
 $content = Get-Content .\knownissues.md -Raw | Convert-Markdown
 Set-Content .\knownissues.html $template.Replace("[Content]", $content)
+
+Pop-Location
