6a5bf027c8bc02e9b9d85de683d48260e2b6d20a
Bobby Cannon <bobby.cannon@live.com>
2017-05-28 09:08:36 -0400

    Added more scripts

---
+++ Deploy.ps1
@@ -0,0 +1,69 @@
+﻿param (
+    [Parameter(Mandatory = $false)]
+    [string] $SitePath = "C:\inetpub\TestR",
+    [Parameter(Mandatory = $false)]
+    [string] $UserName,
+    [Parameter(Mandatory = $false)]
+    [string] $Password
+)
+
+$ErrorActionPreference = "Stop"
+$siteName = "TestR"
+
+Import-Module WebAdministration
+
+$scriptPath = $PSScriptRoot
+if ($scriptPath -eq $null) {
+	$scriptPath = "C:\Workspaces\GitHub\TestR"
+}
+
+if (Test-Path $SitePath -PathType Container) {
+	Remove-Item $SitePath -Recurse
+}
+
+New-Item $SitePath -ItemType Directory | Out-Null
+Copy-Item "$scriptPath\TestR\*" $SitePath -Recurse -Force
+
+$bindings = @()
+$bindings += @{ protocol="http"; bindingInformation="*:80:testr.local"}
+$bindings += @{ protocol="https"; bindingInformation="*:443:testr.local"; sslFlags=1 }
+$webPath = "IIS:\Sites\$siteName"
+$site = Get-Website $siteName
+# Remove-Website $siteName
+
+if (!(Test-Path $SitePath -PathType Container)) {
+	New-Item $SitePath -ItemType Directory
+}
+
+if ($site -eq $null) {
+	New-Website -Name $siteName -PhysicalPath $SitePath
+	$site = Get-Website $siteName
+}
+
+Set-ItemProperty -Path $webPath -Name Bindings -Value $bindings
+
+$certificate = (Get-ChildItem Cert:\LocalMachine -Recurse | Where { $_.Subject -ne $null -and $_.Subject.Contains("testr.local") })
+if ($certificate -eq $null) {
+	$certificate = New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My -DnsName testr.local
+}
+
+$binding = Get-WebBinding -Name $siteName -Protocol "https"
+$binding.AddSslCertificate($certificate.GetCertHashString(), "WebHosting")
+
+$pool = Get-Item "IIS:\AppPools\$siteName" -ErrorAction Ignore
+if ($pool -eq $null) {
+    New-WebAppPool -Name $SiteName
+    $pool = Get-Item IIS:\AppPools\$siteName
+}
+
+Set-ItemProperty IIS:\Sites\$siteName -Name applicationPool -Value $siteName
+
+if (![string]::IsNullOrWhiteSpace($UserName) -and ![string]::IsNullOrWhiteSpace($Password)) {
+	$pool.processModel.userName = $UserName
+	$pool.processModel.password = $Password
+	$pool.processModel.identityType = 3
+	$pool | Set-Item
+}
+
+Start-WebAppPool -Name $siteName
+$site.Start()
---
+++ RunTests.ps1
@@ -0,0 +1,29 @@
+﻿param (
+	[Parameter(Mandatory = $false)]
+	[string] $Test = "TestR.AutomationTests",
+	[Parameter(Mandatory = $false)]
+	[switch] $List
+)
+
+$ErrorActionPreference = "Stop"
+
+$path = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\Extensions\TestPlatform\vstest.console.exe"
+
+if (!(Test-Path $path -PathType Leaf)) {
+	$path = "C:\Program Files (x86)\Microsoft Visual Studio\2017\TestAgent\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+}
+
+if (!(Test-Path $path -PathType Leaf)) {
+	$path = "C:\TestAgent2017\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+}
+
+if (!(Test-Path $path -PathType Leaf)) {
+	throw "Could not find the vstest.console.exe"
+}
+
+$directory = $PSScriptRoot
+$files = Get-ChildItem $directory "*$Test.dll" -File -Recurse
+$filePath = $files[0].FullName
+$filePath
+
+& "$path" "$filePath" $(if ($List.IsPresent) { "-lt" } else { "" })
