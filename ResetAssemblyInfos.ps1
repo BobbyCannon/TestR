137c04adaf6a7241beda861ecbebd9900b7dd53b
Bobby Cannon <bobby.cannon@live.com>
2018-07-08 14:07:46 -0400

    Upgrade some projects
    Rolled back version for next release, opps

--- ResetAssemblyInfos.ps1
+++ ResetAssemblyInfos.ps1
@@ -1,10 +1,11 @@
-﻿$currentPath = (Get-Location -PSProvider FileSystem).ProviderPath
+﻿param ()
+
+$currentPath = (Get-Location -PSProvider FileSystem).ProviderPath
 $assemblyInfos = Get-ChildItem -Recurse -Filter "assemblyinfo.cs"
 
 foreach ($assemblyInfo in $assemblyInfos) {
     Write-Verbose $assemblyInfo.FullName
-    Invoke-Expression ("cd `"" + $assemblyInfo.Directory.FullName + "`"")
-    Invoke-Expression ("git checkout -- " + $assemblyInfo.Name)
+    Invoke-Expression ("git checkout -- " + $assemblyInfo.FullName)
 }
 
 Set-Location $currentPath
