aef5042feedb65acf89e6f913f071a6059b08a9a
Bobby Cannon <bobby.cannon@live.com>
2018-07-08 14:09:34 -0400

    Release for 2.0.18

--- TestR.AutomationTests/Properties/AssemblyInfo.cs
+++ TestR.AutomationTests/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.17.0")]
-[assembly: AssemblyFileVersion("2.0.17.0")]
+[assembly: AssemblyVersion("2.0.18.25437")]
+[assembly: AssemblyFileVersion("2.0.18.25437")]
