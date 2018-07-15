e0b541be03ec47227666a133109d67309ad016ad
Bobby Cannon <bobby.cannon@live.com>
2018-07-15 12:50:56 -0400

    Fixed issues with nuget release

--- TestR.PowerShell/Properties/AssemblyInfo.cs
+++ TestR.PowerShell/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.19.21005")]
-[assembly: AssemblyFileVersion("2.0.19.21005")]
+[assembly: AssemblyVersion("2.0.20.21631")]
+[assembly: AssemblyFileVersion("2.0.20.21631")]
--- TestR.TestSite/Properties/AssemblyInfo.cs
+++ TestR.TestSite/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.19.21005")]
-[assembly: AssemblyFileVersion("2.0.19.21005")]
+[assembly: AssemblyVersion("2.0.20.21631")]
+[assembly: AssemblyFileVersion("2.0.20.21631")]
